608f5218b83a3d43ecade370f40b564bf3f62465
Martin Manzur <martin.manzur@incluit.com>
2022-12-13 12:08:41 -0300

    deleted example code

--- new_vehicle_recognition_dummy.py
+++ new_vehicle_recognition_dummy.py
@@ -5,6 +5,17 @@ import numpy as np
 
 video_path = "./video/in.avi"
 
+
+
 def main():
+    #paso 1 capturar frame
     vidcap = cv2.VideoCapture(video_path)
-    
+    success, img = vidcap.read()
+    count = 0
+    success = True
+    while success:
+        success, image = vidcap.read()
+        cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+        if cv2.waitKey(10) == 27:                     # exit if Escape is hit
+            break
+        count += 1
